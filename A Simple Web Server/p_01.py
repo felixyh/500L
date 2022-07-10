5a1a7749fbcbcd9e1c2d4854da1680f136aecfea
felixyh <felix_yanghui@sina.cn>
2022-07-10 14:48:33 +0800

    update @Jul, 10

--- A Simple Web Server/p_01.py
+++ A Simple Web Server/p_01.py
@@ -8,11 +8,11 @@
 # send it back.
 
 # in Python 3 which moved BaseHTTPServer to http.server.HTTPServer
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import requests
+
+import http.server as h
 
 
-class RequestHandler(BaseHTTPRequestHandler):
+class RequestHandler(h.BaseHTTPRequestHandler):
     """ Handle HTTP Requests by returning a fixed 'page' """
 
     # Page to send back
@@ -24,18 +24,21 @@ class RequestHandler(BaseHTTPRequestHandler):
     </html>
     '''
 
-    # Handle a Get Request
-    def do_Get(self):
+    # Handle a Get Request, 重写do_GET() 方法
+    def do_GET(self):
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.send_header("Content-Length", str(len(self.Page)))
         self.end_headers()
-        self.wfile.write(self.Page)
+        # encode 把字符串转换成字节
+        self.wfile.write(self.Page.encode())
+
+# ----------------------------------------------------------------------
 
 
 if __name__ == '__main__':
     serverAddress = ('', 8080)
-    server = HTTPServer(serverAddress, RequestHandler)
+    server = h.HTTPServer(serverAddress, RequestHandler)
     server.serve_forever()
 
 
---
+++ A Simple Web Server/p_03.py
@@ -0,0 +1,3 @@
+# Serving Static Pages
+# serving pages from the disk instead of generating them on the fly. We'll start by rewriting do_GET
+
