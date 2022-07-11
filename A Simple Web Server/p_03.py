3d07b01a13721c9452b8aebf7996771e2e07896f
felixyh <felixyanghui@gmail.com>
2022-07-11 21:34:26 +0800

    update @Jul, 10

--- A Simple Web Server/p_03.py
+++ A Simple Web Server/p_03.py
@@ -1,16 +1,15 @@
 # Serving Static Pages
 # serving pages from the disk instead of generating them on the fly. We'll start by rewriting do_GET
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler
 import os
 
 
-class ServerException(BaseException):
+class ServerException:
     pass
 
 
 class RequestHandler(BaseHTTPRequestHandler):
-
     def do_GET(self):
         try:
             # Figure out what exactly is being requested
@@ -29,44 +28,11 @@ class RequestHandler(BaseHTTPRequestHandler):
                 raise ServerException("Unknown object '{0}'".format(self.path))
             
         # Handle errors.
-        except ServerException as msg:
+        except Exception as msg:
             self.handle_error(msg)
-
-    def handle_file(self, full_path):
-        try:
-            with open(full_path, 'rb') as reader:
-                content = reader.read()
-            self.send_content(content)
-        except IOError as msg:
-            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
-            self.handle_error(msg)
-
-    # handle unknown objects
-    Error_Page = """\
-        <html>
-        <body>
-        <h1>Error accessing {path}</h1>
-        <p>{msg}</p>
-        </body>
-        </html>
-        """
 
     def handle_error(self, msg):
-        content = self.Error_Page.format(path=self.path, msg=msg)
-
-        # python3 send_content 只能接受字节，需要encode() 把字符串转换成字节
-        self.send_content(content.encode(), 404)
-
-    # send actual content
-    def send_content(self, content, status=200):
-        self.send_response(status)
-        self.send_header("Content-type", "text/html")
-        self.send_header("Content-Length", str(len(content)))
-        self.end_headers()
-        self.wfile.write(content)
+        pass
 
 
-if __name__ == '__main__':
-    serverAddress = ('', 8080)
-    server = HTTPServer(serverAddress, RequestHandler)
-    server.serve_forever()
+
